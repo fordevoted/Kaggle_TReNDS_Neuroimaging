8cc5f95ed1364b43b3cad6a1149e2ef372493d9e
Yu-Wei, Chen <210509fssh@gmail.com>
2020-06-24 13:54:03 +0800

    update pca_baseline.py

--- pca_baseline.py
+++ pca_baseline.py
@@ -2,12 +2,14 @@
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # import plotly.offline as py
 # py.init_notebook_mode(connected=True)
+from scipy.special.cython_special import boxcox1p
 from tensorflow.python.keras import regularizers
 
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv3D, Dense, Dropout, BatchNormalization, Activation
 import tensorflow.keras.backend as K
 
+from scipy.stats import skew
 from sklearn.impute import KNNImputer
 import tensorflow as tf
 from keras.callbacks import ModelCheckpoint
@@ -36,13 +38,117 @@ for i in range(0,len(sample_submission.Id),5):
     test_id.append(float(sample_submission.Id[i].split("_")[0]))
 test = pd.DataFrame(test_id, columns=["Id"]).merge(data, on='Id')
 
-X_train1, X_pretest1, y_train1, y_pretest1 = train_test_split(train.drop(['Id'], axis=1), pd.DataFrame(train_scores).drop('Id', axis=1), test_size=0.2, random_state=42)
+train = train.drop(['Id'], axis=1)
+label = pd.DataFrame(train_scores).drop('Id', axis=1)
+ntrain = train.shape[0]
 
 impute = KNNImputer(n_neighbors=40)
-y_train2 = impute.fit_transform(y_train1)
-y_pretest2 = impute.transform(y_pretest1)
+label = impute.fit_transform(label)
+
+all_data = pd.concat([train, test], axis=1)
 
 scaler = StandardScaler()
+X_train = scaler.fit_transform(all_data)
+print(X_train.shape)
+
+pca = KernelPCA(n_components=900, kernel='cosine')# more than 0.95
+all_data = pca.fit_transform(all_data)
+
+
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+
+X_train, X_validation, y_train, y_validation = train_test_split(train, label, test_size=0.2, random_state=42)
+
+
+input_dim = X_train.shape[1]
+regularize_rate = 5.0e-5
+# model = Sequential()
+model = Sequential()
+# input
+# model.add(Convolution1D(nb_filter=512, filter_length=1, input_shape=(input_dim, )))
+# model.add(BatchNormalization())
+model.add(Dense(512, kernel_initializer="lecun_normal", input_shape=(input_dim,)))
+# model.add(BatchNormalization())
+model.add(Activation('selu'))
+model.add(Dropout(0.4))
+model.add(Dense(512, kernel_initializer="lecun_normal",
+                kernel_regularizer=regularizers.l2(regularize_rate)))
+# model.add(BatchNormalization())
+model.add(Activation('selu'))
+model.add(Dropout(0.4))
+
+model.add(Dense(256, kernel_initializer="lecun_normal",
+                kernel_regularizer=regularizers.l2(regularize_rate)))
+# model.add(BatchNormalization())
+model.add(Activation('selu'))
+model.add(Dropout(0.2))
+
+# model.add(Dense(128, kernel_initializer="lecun_normal",
+#                 kernel_regularizer=regularizers.l2(regularize_rate)))
+# model.add(BatchNormalization())
+# model.add(Activation('selu'))
+# model.add(Dropout(0.4))
+model.add(Dense(5, activation='relu'))
+
+model = model
+model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.0001), metrics=[ weighted_NAE])
+model.summary()
+
+filepath = "./best.hdf5"
+
+checkpoint = ModelCheckpoint(filepath,
+                            monitor='val_weighted_NAE',
+                            verbose=1,
+                            save_best_only=True,
+                            mode='min',
+                             save_weights_only=True)
+history = model.fit(X_train, y_train, validation_data=(X_validation,y_validation), epochs=30, batch_size=16, callbacks=[checkpoint])
+model.load_weights(filepath)
+
+pred=pd.DataFrame()
+pred["Id"]=sample_submission.Id
+pred["Predicted"]=model.predict(test).flatten()
+pred.to_csv('out2.csv', index=False)
+
+# plt.plot(history.history['weighted_NAE'])
+plt.plot(history.history['val_weighted_NAE'])
+plt.title('model weighted_NAE')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['test'], loc='upper left')
+plt.show()
+
+
+
+'''
+0.1658
+model.add(BatchNormalization(input_shape=(input_dim,)))
+model.add(Dense(512, kernel_initializer="lecun_normal", use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation('selu'))
+# model.add(Dropout(0.2))
+
+model.add(Dense(1024, kernel_initializer="lecun_normal", use_bias=False,
+                kernel_regularizer=regularizers.l2(regularize_rate)))
+model.add(BatchNormalization())
+model.add(Activation('selu'))
+# model.add(Dropout(0.2))
+
+model.add(Dense(2048, kernel_initializer="lecun_normal", use_bias=False,
+                kernel_regularizer=regularizers.l2(regularize_rate)))
+model.add(BatchNormalization())
+model.add(Activation('selu'))
+# model.add(Dropout(0.4))
+
+model.add(Dense(256, kernel_initializer="lecun_normal", use_bias=False,
+                kernel_regularizer=regularizers.l2(regularize_rate)))
+model.add(BatchNormalization())
+model.add(Activation('selu'))
+# model.add(Dropout(0.2))
+model.add(Dense(5, activation='relu'))
+
+'''
 train2 = scaler.fit_transform(X_train1)
 pretest2 = scaler.transform(X_pretest1)
 test2 = scaler.transform( test.drop(['Id'], axis=1))
